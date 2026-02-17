1d91de4b9cca32dc3ccac9398e542c71df473899
Fayber <deine@email.de>
2026-02-17 21:55:59 +0100

    Made the smooth look better

--- components/looker.py
+++ components/looker.py
@@ -2,26 +2,41 @@ import system.lib.minescript as minescript
 import random
 import time
 
-CONFIRM_YAW_PITCH = True # If it should manuallly correct at the end (If you need exact Yaw and Pitch)
+CONFIRM_YAW_PITCH = False # If it should manuallly correct at the end (If you need exact Yaw and Pitch
 
-def look(target_yaw, target_pitch, duration=0.4, steps=20):
+def look(target_yaw, target_pitch, duration=0.22, steps=70):
     sy, sp = minescript.player_orientation()
-    dy, dp = target_yaw - sy, target_pitch - sp
+
+    def angle_diff(a, b):
+        return (b - a + 180) % 360 - 180
+
+    dy = angle_diff(sy, target_yaw)
+    dp = target_pitch - sp
+
+    if abs(dy) < 1.0 and abs(dp) < 1.0:
+        minescript.player_set_orientation(target_yaw, target_pitch)
+        return
+
+    step_time = duration / steps
+
+    power = 5
 
     for i in range(1, steps + 1):
         t = i / steps
-        s = 1 - (1 - t) ** 3
 
-        jy = (1 - t) * 0.6
+        if t < 0.5:
+            s = 0.5 * (2 * t) ** power
+        else:
+            s = 1 - 0.5 * (2 * (1 - t)) ** power
+
+        jy = (1 - abs(0.5 - t) * 2) * 0.2
+
         minescript.player_set_orientation(
             sy + dy * s + random.uniform(-jy, jy),
             sp + dp * s + random.uniform(-jy * 0.7, jy * 0.7)
         )
-        time.sleep(duration / steps)
 
-    minescript.player_set_orientation(
-        target_yaw + random.uniform(-0.15, 0.15),
-        target_pitch + random.uniform(-0.15, 0.15)
-    )
+        time.sleep(step_time)
 
-    minescript.player_set_orientation(target_yaw, target_pitch)
+    if CONFIRM_YAW_PITCH:
+        minescript.player_set_orientation(target_yaw, target_pitch)

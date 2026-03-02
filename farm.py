1dc42f89c3b39a8abff54d2f6883e04cc77c38bf
XyferCry <xyfercry@gmail.com>
2026-03-02 17:02:19 +0100

    Update farm.py with fixed error handling

--- farm.py
+++ farm.py
@@ -3,52 +3,53 @@ import system.lib.minescript as m
 import time, os, traceback, random, winsound, mss, requests, pygetwindow as gw, tempfile, threading, asyncio
 
 from components.ms_extended import look, get_scoreboard_info
-from config import discord_webhook_url #Needs to be imported from a config.py as a discord_webhook_url variable
+from config import discord_webhook_url  # Needs to be imported from a config.py as a discord_webhook_url variable
 
 # ========== PATHS ==========
 BASE_DIR = os.path.dirname(__file__)
-LOG_PATH = os.path.join(BASE_DIR, "FarmLog.log") #File Name in which the logs will be saved
+LOG_PATH = os.path.join(BASE_DIR, "FarmLog.log")  # File Name in which the logs will be saved
 
-WARN_SOUND_PATH = os.path.join(BASE_DIR,"assets","AnvilLand.wav") #Set this to your Warn sound file path (Default is a relative folder with the sound in it)
+WARN_SOUND_PATH = os.path.join(BASE_DIR, "assets",
+                               "AnvilLand.wav")  # Set this to your Warn sound file path (Default is a relative folder with the sound in it)
 
 # ========== CONFIG ==========
-PAUSE_KEY = 320 #Numpad0
-WARP_KEY = 330 #NumpadComma
-SET_ORI_KEY = 260 #INSERT Key
-END_KEY = 269 #END Key
+PAUSE_KEY = 320  # Numpad0
+WARP_KEY = 330  # NumpadComma
+SET_ORI_KEY = 260  # INSERT Key
+END_KEY = 269  # END Key
 
-FARM_ITEM = "diamond_axe" #The Tool that is used for farming (Minecraft ID, not hypixel name)
-FARM_BLOCK = "melon" #The block/crop that is being farmed
+FARM_ITEM = "diamond_axe"  # The Tool that is used for farming (Minecraft ID, not hypixel name)
+FARM_BLOCK = "melon"  # The block/crop that is being farmed
 
-ROW_MIN_Z = -238.68 #Y Coordinate of the Start of a row
-ROW_MAX_Z = 238.68 #Y Coordinate of the Start of a row
+ROW_MIN_Z = -238.68  # Y Coordinate of the Start of a row
+ROW_MAX_Z = 238.68  # Y Coordinate of the Start of a row
 
-ROW_MAX_X = -55.3 #X Coordinate of the last row
-ROW_MIN_X = -88.3 #X Coordinate of the beginning Row
+ROW_MAX_X = -55.3  # X Coordinate of the last row
+ROW_MIN_X = -88.3  # X Coordinate of the beginning Row
 
-PUSH_MIN = 0.1 #Minimum time in seconds the script pushes against a row end to look more human
-PUSH_MAX = 1.5  #Maximum push Time
+PUSH_MIN = 0.1  # Minimum time in seconds the script pushes against a row end to look more human
+PUSH_MAX = 1.5  # Maximum push Time
 
-FARM_HEIGHT_Y = 67.875 #The height of walking through the rows
-FARM_HEIGHT_TOL = 0.3 #Small tolerance for float inconsistencies
+FARM_HEIGHT_Y = 67.875  # The height of walking through the rows
+FARM_HEIGHT_TOL = 0.3  # Small tolerance for float inconsistencies
 
-END_TOL = 0.1 #Small Tolerance to evade float inconsistencies
+END_TOL = 0.1  # Small Tolerance to evade float inconsistencies
 
-END_HARD_WAIT = 2.0 #Minimum Wait before warping
-END_EXTRA_MIN = 1.0 #Mininimum Extra Wait before warping
-END_EXTRA_MAX = 2.0 #Maximum Extra Wait before warping
+END_HARD_WAIT = 2.0  # Minimum Wait before warping
+END_EXTRA_MIN = 1.0  # Mininimum Extra Wait before warping
+END_EXTRA_MAX = 2.0  # Maximum Extra Wait before warping
 
-WARP_WAIT = 1.0 #Time to wait for warping to load
-POST_WARP_MIN = 0.75 #Minimum Wait after auto warp
-POST_WARP_MAX = 1.0 #Maximum Wait after auto warp
+WARP_WAIT = 1.0  # Time to wait for warping to load
+POST_WARP_MIN = 0.75  # Minimum Wait after auto warp
+POST_WARP_MAX = 1.0  # Maximum Wait after auto warp
 
-farm_speed = 265 #Your Speed cap while farming (Use Sundial)
-farm_pet = "Elephant" #Pet you use while farming (Without Level. Just for example "Elephant")
+farm_speed = 265  # Your Speed cap while farming (Use Sundial)
+farm_pet = "Elephant"  # Pet you use while farming (Without Level. Just for example "Elephant")
 
 LAST_POS = 0
-webhook_alert = True #If Alerts should be sent to Webhook
+webhook_alert = True  # If Alerts should be sent to Webhook
 
-auto_restart_after_evac = True #If the Script should automatically begin farming again after a Server restart or Limbo Kick
+auto_restart_after_evac = True  # If the Script should automatically begin farming again after a Server restart or Limbo Kick
 # ========== STATE ==========
 paused = True
 running = True
@@ -67,9 +68,11 @@ scoreboard_lock = threading.Lock()
 with open(LOG_PATH, "w", encoding="utf-8") as f:
     f.write("=== FARM BASE LOG START ===\n")
 
+
 def log(msg: str):
     with open(LOG_PATH, "a", encoding="utf-8") as f:
         f.write(f"[{time.strftime('%H:%M:%S')}] {msg}\n")
+
 
 def log_state(tag: str):
     try:
@@ -83,6 +86,8 @@ def log_state(tag: str):
         )
     except Exception as e:
         log(f"STATE ERROR: {e}")
+
+
 # ================= Helper =================
 def look_async(yaw, pitch, duration=0.4, steps=20):
     threading.Thread(
@@ -90,6 +95,7 @@ def look_async(yaw, pitch, duration=0.4, steps=20):
         args=(yaw, pitch, duration, steps),
         daemon=True
     ).start()
+
 
 def scoreboard_updater():
     global scoreboard_cache
@@ -107,12 +113,13 @@ def scoreboard_updater():
 
         time.sleep(0.1)
 
+
 def get_scoreboard_cached():
     with scoreboard_lock:
         return scoreboard_cache.copy()
 
-def alert(alert_msg, sound, send_screenshot):
 
+def alert(alert_msg, sound, send_screenshot):
     log(f"[ALERT] {alert_msg}")
 
     m.echo(f"[ALERT] §c{alert_msg}")
@@ -142,8 +149,10 @@ def play_sound(sound_path):
     except Exception as e:
         m.echo(f"§c[ERROR] {e}§r")
 
+
 def beep():
     winsound.Beep(1000, 300)
+
 
 def player_items():
     items = m.player_hand_items()
@@ -152,6 +161,7 @@ def player_items():
     offhand_item = items.off_hand
 
     return mainhand_item, offhand_item
+
 
 def failsafe():
     global farm_speed, farm_pet
@@ -164,22 +174,19 @@ def failsafe():
     if not scoreboard or scoreboard == {}:
         return False, "Scoreboard empty", "None"
 
-    if all(value is None for value in scoreboard.values()):
-        return False, "Scoreboard Values all None", "None"
-
-    if not scoreboard.get("area") == "Garden":
+    if not scoreboard.get("area") == "Garden" and not scoreboard.get("area") is None:
         log(f"""[FAILSAFE] Detected Area not Garden. Scoreboard Info
 {scoreboard}
 """)
         return True, "Area not Garden", "default"
 
-    if not scoreboard.get("speed") == farm_speed:
+    if not scoreboard.get("speed") == farm_speed and not scoreboard.get("speed") is None:
         log(f"""[FAILSAFE] Detected Speed not being {farm_speed}. Scoreboard Info
 {scoreboard}
 """)
         return True, f"Speed not {farm_speed}", "default"
 
-    if not scoreboard.get("pet_name") == farm_pet:
+    if not scoreboard.get("pet_name") == farm_pet and not scoreboard.get("pet_name") == None:
         log(f"""[FAILSAFE] Detected Pet not being {farm_pet}. Scoreboard Info
 {scoreboard}
 """)
@@ -193,7 +200,9 @@ def failsafe():
         log("[FAILSAFE] Detected wrong Orientation")
         return True, "Orientation not correct", "beep"
 
-    if not (ROW_MIN_X-2 <= x < ROW_MAX_X) or not (FARM_HEIGHT_Y-FARM_HEIGHT_TOL <= y < FARM_HEIGHT_Y+FARM_HEIGHT_TOL) or not (ROW_MIN_Z-2 <= z < ROW_MAX_Z+2):
+    if not (ROW_MIN_X - 2 <= x < ROW_MAX_X) or not (
+            FARM_HEIGHT_Y - FARM_HEIGHT_TOL <= y < FARM_HEIGHT_Y + FARM_HEIGHT_TOL) or not (
+            ROW_MIN_Z - 2 <= z < ROW_MAX_Z + 2):
         log("[FAILSAFE] Detected wrong Coordinates (OUT OF FARM)")
         return True, "Coordinates outside of Farm", "default"
 
@@ -202,6 +211,7 @@ def failsafe():
         return True, "Invalid X Coordinate", "beep"
 
     return False, "None", "None"
+
 
 def webhook(content, send_screenshot=False):
     if not discord_webhook_url:
@@ -229,6 +239,7 @@ def webhook(content, send_screenshot=False):
 
     threading.Thread(target=worker, daemon=True).start()
 
+
 def webhook_is_valid():
     if not discord_webhook_url:
         log("[WEBHOOK] Webhook url is None")
@@ -251,11 +262,11 @@ def webhook_is_valid():
 def stop_inputs():
     global attack_held
     for fn in (
-        m.player_press_attack,
-        m.player_press_forward,
-        m.player_press_backward,
-        m.player_press_left,
-        m.player_press_right,
+            m.player_press_attack,
+            m.player_press_forward,
+            m.player_press_backward,
+            m.player_press_left,
+            m.player_press_right,
     ):
         try:
             fn(False)
@@ -263,10 +274,12 @@ def stop_inputs():
             log(f"stop_inputs error: {e}")
     attack_held = False
 
+
 def set_move(forward=False, left=False, right=False):
     m.player_press_forward(forward)
     m.player_press_left(left)
     m.player_press_right(right)
+
 
 # ================= DIRECTION =================
 def get_direction(x: float):
@@ -280,8 +293,10 @@ def get_direction(x: float):
     )
     return direction, snapped_x
 
+
 def at_field_end(x, z) -> bool:
     return abs(x - ROW_MAX_X) < END_TOL and abs(z - ROW_MAX_Z) < END_TOL
+
 
 # ================= ACTIONS =================
 def is_valid_row_x(x: float) -> bool:
@@ -289,12 +304,14 @@ def is_valid_row_x(x: float) -> bool:
     snapped_x = ROW_MIN_X + row_index * 3
     return abs(x - snapped_x) < 0.05
 
+
 def ensure_attack():
     global attack_held
     if not attack_held:
         m.player_press_attack(True)
         attack_held = True
         log("[ATTACK] locked ON")
+
 
 def toggle_pause():
     global paused, attack_held
@@ -316,7 +333,9 @@ def toggle_pause():
         log(f"[PAUSE] ERROR reading state: {e}")
         return
 
-    if not (ROW_MIN_X-2 <= x < ROW_MAX_X) or not (FARM_HEIGHT_Y-FARM_HEIGHT_TOL <= y < FARM_HEIGHT_Y+FARM_HEIGHT_TOL) or not (ROW_MIN_Z-2 <= z < ROW_MAX_Z+2):
+    if not (ROW_MIN_X - 2 <= x < ROW_MAX_X) or not (
+            FARM_HEIGHT_Y - FARM_HEIGHT_TOL <= y < FARM_HEIGHT_Y + FARM_HEIGHT_TOL) or not (
+            ROW_MIN_Z - 2 <= z < ROW_MAX_Z + 2):
         log(f"[PAUSE] Outside Farm")
         try:
             m.echo("[HyFarmer] §eError unpausing: Outside of Farm")
@@ -348,8 +367,6 @@ def toggle_pause():
             pass
         return
 
-
-
     paused = False
     ensure_attack()
     log("[PAUSE] RESUME (attack locked)")
@@ -357,6 +374,7 @@ def toggle_pause():
         m.echo("[HyFarmer] Resumed")
     except Exception:
         pass
+
 
 def do_warp():
     log("[WARP] /warp garden")
@@ -366,6 +384,7 @@ def do_warp():
         pass
     stop_inputs()
     m.execute("/warp garden")
+
 
 # ================= ORIENTATION =================
 def set_orientation():
@@ -378,12 +397,14 @@ def set_orientation():
         pass
     m.player_set_orientation(TARGET_YAW, TARGET_PITCH)
 
+
 # ================= JOB CONTROL =================
 def kill_all_jobs():
     log("=== KILL_ALL START ===")
     m.echo("[HyFarmer] Stopping all jobs...")
     stop_inputs()
     m.execute(r"\killjob -1")
+
 
 # ================= LISTENER =================
 def on_key(event):
@@ -393,6 +414,7 @@ def on_key(event):
     k = event["key"]
     log(f"[KEY] {k}")
     _last_key_seen = k
+
 
 def on_chat(event):
     global pause_script, warp_and_resume
@@ -468,7 +490,8 @@ def restart_after_evac():
     m.log(f"[EVAC_RESTARTER] Unpausing in Garden at coords: {m.player_position()}")
     toggle_pause()
 
-#Listeners and Thread Workers
+
+# Listeners and Thread Workers
 m._register_chat_message_listener(on_chat)
 m._register_key_listener(on_key)
 
@@ -489,7 +512,6 @@ if not os.path.exists(WARN_SOUND_PATH):
     WARN_SOUND_PATH = None
 else:
     m.echo("[HyFarmer] §aVerified Warning sound File")
-
 
 log_state("START")
 
@@ -516,7 +538,7 @@ while running:
                 kill_all_jobs()
                 os._exit(0)
 
-            if k == 334: #numpadPlus
+            if k == 334:  # numpadPlus
                 warp_and_resume = True
 
         if pause_script:
@@ -525,7 +547,7 @@ while running:
             continue
 
         if warp_and_resume:
-           restart_after_evac()
+            restart_after_evac()
 
         if paused:
             time.sleep(0.05)
@@ -540,13 +562,10 @@ while running:
         if start_row_x is None:
             start_row_x = snapped_x
 
-
-
         failsafe_result = failsafe()
 
-
         if failsafe_result[0]:
-            #m.echo(failsafe_result)
+            # m.echo(failsafe_result)
             alert(failsafe_result[1], failsafe_result[2], True)
 
         if STATE == "FARM_ROW":
@@ -561,7 +580,7 @@ while running:
                 if (LAST_POS == m.player_position()) and not at_wall:
                     alert("NO MOVEMENT DETECTED !!!!!!!!!!!!!!!!!!!!!!!!!", "default")
                 else:
-                    #m.echo(f"Last POS: {LAST_POS} Cur Pos: {m.player_position()}")
+                    # m.echo(f"Last POS: {LAST_POS} Cur Pos: {m.player_position()}")
                     LAST_POS = m.player_position()
 
             else:
@@ -577,8 +596,8 @@ while running:
                     pass
 
             at_wall = (
-                (direction == "left" and z <= ROW_MIN_Z) or
-                (direction == "right" and z >= ROW_MAX_Z)
+                    (direction == "left" and z <= ROW_MIN_Z) or
+                    (direction == "right" and z >= ROW_MAX_Z)
             )
 
             if at_wall and row_push_until == 0.0:
